a78b03c80ccba66986fbfeb37c49fae459101313
Đăng Châu <chauhaidang1@gmail.com>
2019-06-15 16:41:22 +0700

    Config to run on chrome directly

--- conf.js
+++ conf.js
@@ -1,4 +1,11 @@
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['todo-spec.js']
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['todo-spec.js'],
+    directConnect: true,
+    capabilities: {
+      browserName: 'chrome',
+      chromeOptions: {
+        //args: ['--headless']
+      }
+    }
   };
