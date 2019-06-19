f88e0b3fc1404d9aae2aeaafd6fb8b7327799687
Đăng Châu <chauhaidang1@gmail.com>
2019-06-20 02:24:50 +0700

    Integrate html reporter 2

--- conf.js
+++ conf.js
@@ -1,68 +1,100 @@
-let {logger,transports} = require('./util/LogSupport');
+let { logger, transports } = require('./util/LogSupport');
 let dateformat = require('dateformat');
 let fs2 = require('fs-extra');
+let HTMLReport = require('protractor-html-reporter-2');
+let jasmineReporters = require('jasmine-reporters');
+let path = require('path');
 
 const _ReportDir = 'report/';
 let reportNameSpace;
 
 exports.config = {
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
-    framework: 'jasmine2',
-    jasmineNoteOpts:{
-        defaultTimeoutInterval: 30000,
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  framework: 'jasmine2',
+  jasmineNoteOpts: {
+    defaultTimeoutInterval: 30000,
+  },
+
+  specs: ['./training/protractor_tut/sample_log.js'],
+  suites: {
+    smoke: ['./suite/smoke/*.js'],
+    functional: ['./suite/functional/*.js'],
+    regression: ['./suite/regression/*.js'],
+    all: ['./*/*/*.js']
+  },
+
+  directConnect: true,
+
+  capabilities: {
+    // browserName: 'firefox',
+    // 'moz:firefoxOptions': {
+    //   args: ['--headless', '--safe-mode']
+    // }
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless']
     },
+  },
 
-    specs: ['./training/protractor_tut/sample_log.js'],
-    suites: {
-      smoke: ['./suite/smoke/*.js'],
-      functional: ['./suite/functional/*.js'],
-      regression: ['./suite/regression/*.js'],
-      all: ['./*/*/*.js']
-    },
+  getPageTimeout: 10000,
 
-    directConnect: true,
-    
-    capabilities: {
-      // browserName: 'firefox',
-      // 'moz:firefoxOptions': {
-      //   args: ['--headless', '--safe-mode']
-      // }
-      browserName: 'chrome',
-      chromeOptions: {
-        args: ['--headless']
-      },
-    },
+  restartBrowserBetweenTests: true,
 
-    getPageTimeout: 10000,
+  SELENIUM_PROMISE_MANAGER: true,
 
-    restartBrowserBetweenTests: true,
+  beforeLaunch: () => {
+    //clean up report dir
+    fs2.emptyDir(_ReportDir).then(() => {
+      console.log('success clean up report folder before run tests!');
+    }).catch(err => {
+      console.error(err);
+    });
+  },
 
-    SELENIUM_PROMISE_MANAGER: true,
+  onPrepare: () => {
+    browser.manage().window().maximize();
+    browser.driver.manage().timeouts().implicitlyWait(60000);//This apply for non-angular page and wait for statement to be timeout implicitly
 
-    beforeLaunch: () => {
-      //clean up report dir
-      fs2.emptyDir(_ReportDir).then(()=>{
-        console.log('success clean up report folder before run tests!');
-      }).catch(err => {
-        console.error(err);
-      });
-    },
+    let date = new Date();
+    reportNameSpace = dateformat(date, 'dddd_mmmm_dS_yyyy_HH_MM_ss');
 
-    onPrepare: () => {
-      browser.manage().window().maximize();
-      browser.driver.manage().timeouts().implicitlyWait(5000);//This apply for non-angular page and wait for statement to be timeout implicitly
+    logger.add(
+      new transports.File({
+        filename: `${__dirname}/${_ReportDir}${reportNameSpace}_ExecutionLog.log`,
+      })
+    );
 
-      let date = new Date();
-      reportNameSpace = dateformat(date, 'dddd_mmmm_dS_yyyy_HH_MM_ss');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: `${_ReportDir}`,
+      filePrefix: `${reportNameSpace}_xmloutput`
+    }));
 
-      logger.add(
-        new transports.File({
-          filename: `${__dirname}/${_ReportDir}${reportNameSpace}/${reportNameSpace}_ExecutionLog.log`,
-        })
-      );
-      
-      browser.logger = logger;
-      browser.timeouts = 5000;
+    browser.logger = logger;
+    browser.timeouts = 5000;
+  },
 
-    }
-  };
+  onComplete: () => {
+    let browserName, browserVersion;
+    let capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      let platform = caps.get('platform');
+      let testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: `${_ReportDir}`,
+        outputFilename: `${reportNameSpace}_ProtractorTestReport`,
+        screenshotPath: `${_ReportDir}screenshots`,
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: true,
+        screenshotsOnlyOnFailure: false,
+        testPlatform: platform,
+        consolidateAll: true,
+      };
+      new HTMLReport().from(`${_ReportDir}${reportNameSpace}xmloutput.xml`, testConfig);
+    });
+  }
+};
